a12c414d5e3b4f53bb213f0af39814be7de9085d
Christiaan Dotze <christiaan.dotze2@dxc.com>
2025-05-06 09:45:36 +0200

    base idea-service

---
+++ idea-service/src/main/java/za/co/novaflow/idea_service/IdeaServiceApplication.java
@@ -0,0 +1,13 @@
+package za.co.novaflow.idea_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaServiceApplication.class, args);
+	}
+
+}
---
+++ idea-service/src/test/java/za/co/novaflow/idea_service/IdeaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.novaflow.idea_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
